15f7c03b3556e52701cfa591916feae1917e08a2
s.claes <Sadrian2711>
2021-09-16 09:42:53 +0200

    [ADD] control structure system variable __main__

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import json
 
 k = krakenAPI(kraken_api)
 df, last = kraken.get_ohlc_data('BCHUSD', ascending=True)
+
+
+
+if __name__ == '__main__':
+    k = krakenex.API()
+    kraken.load_key('kraken.key')
+    pairs = get_pairs()
+    since = str(int(time.time() - 43200))
+    mva = load_crypto_data_from_file()
+
+    bot(since, k, pairs)
